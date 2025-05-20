254720a3142c033bb7956125284146ef59963f6b
Shubham Prajapati <shubham528prajapati@gmail.com>
2025-05-20 18:59:28 +0530

    change done

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -4,15 +4,35 @@ const Inventory = require("../models/Inventory");
 const createInventory = async (req, res) => {
   try {
     const { inventoryName, companyId } = req.body;
+
+    // Check if user is a manager
+    if (req.user.type !== "manager") {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: Only managers can create inventory" });
+    }
+
+    // Check if company belongs to the manager
+    // const companyExists = req.user.user.companies.includes(companyId);
+    // if (!companyExists) {
+    //   return res.status(403).json({
+    //     message: "Forbidden: Company does not belong to this manager",
+    //   });
+    // }
+
     const newInventory = new Inventory({
       inventoryName,
       company: companyId,
     });
+
     await newInventory.save();
-    const updateCompany = await Company.findByIdAndUpdate(companyId, {
-      $push: { inventory: newInventory._id },
-    });
-    res.status(201).json(newInventory);
+    const updateCompany = await Company.findByIdAndUpdate(
+      companyId,
+      { inventory: newInventory._id },
+      { new: true }
+    );
+
+    res.status(201).json(updateCompany);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
